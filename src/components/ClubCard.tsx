725e6e82776422cff2aa9d41c01a73d943cdc2d6
Jericho Opsima <jerichoopsima22@gmail.com>
2022-09-23 20:11:19 +0800

    refactor

--- src/components/ClubCard.tsx
+++ src/components/ClubCard.tsx
@@ -14,65 +14,63 @@ export default function ClubCard() {
 	return (
 		<>
 			<Box
-				borderWidth={4}
-				borderColor={"black"}
-				borderRadius={20}
-				backgroundColor={"#FFF6EA"}
-				h={650}
-				w={430}
-				p={6}
-				display={"flex"}
-				flexDirection={"column"}
-				justifyContent={"space-between"}
+				borderWidth="4"
+				borderColor="black"
+				borderRadius="20"
+				backgroundColor="#FFF6EA"
+				h="650"
+				w="430"
+				p="6"
+				display="flex"
+				flexDirection="column"
+				justifyContent="space-between"
 			>
 				<Box>
 					<Box>
-						<Box borderRadius={20} borderWidth={2} borderColor={"black"}>
+						<Box borderRadius="20" borderWidth="2" borderColor="black">
 							<Img
-								src={"/club_assets/gdsc-uic2/gdsc-uic-cover-photo.png"}
-								alt={"gdsc-uic"}
-								h={200}
-								w={500}
-								borderRadius={20}
-								objectFit={"cover"}
+								src="/club_assets/gdsc-uic2/gdsc-uic-cover-photo.png"
+								alt="gdsc-uic"
+								h="200"
+								w="500"
+								borderRadius="20"
+								objectFit="cover"
 							/>
 						</Box>
 
 						<Center>
 							<Img
-								src={"/club_assets/gdsc-uic2/logo.png"}
-								alt={"gdsc-logo"}
-								objectFit={"cover"}
-								h={120}
-								mt={-50}
+								src="/club_assets/gdsc-uic2/logo.png"
+								alt="gdsc-logo"
+								objectFit="cover"
+								h="120"
+								mt="-50"
 							/>
 						</Center>
 					</Box>
-					<Box textAlign={"center"}>
-						<Heading fontSize={"1.7rem"}>
-							Google Developer Students Club
-						</Heading>
+					<Box textAlign="center">
+						<Heading fontSize="1.7rem">Google Developer Students Club</Heading>
 						<Text>lorem ipsum dolor sit amet consetectur</Text>
 					</Box>
 				</Box>
 
-				<Flex flexDirection={"column"} gap={4}>
+				<Flex flexDirection="column" gap="4">
 					<Button
-						textColor={"white"}
-						borderRadius={20}
-						borderWidth={2}
+						textColor="white"
+						borderRadius="20"
+						borderWidth="2"
 						p="7"
-						borderColor={"black"}
-						backgroundColor={"#0057FF"}
+						borderColor="black"
+						backgroundColor="#0057FF"
 					>
 						Interested
 					</Button>
 					<Button
-						borderRadius={20}
-						p={7}
-						borderWidth={2}
-						borderColor={"#0057FF"}
-						backgroundColor={"transparent"}
+						borderRadius="20"
+						p="7"
+						borderWidth="2"
+						borderColor="#0057FF"
+						backgroundColor="transparent"
 					>
 						View Club
 					</Button>
