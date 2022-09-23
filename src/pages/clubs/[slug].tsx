0ec2960917c2aff8fec32d18714a24f60c7ec991
Jericho Opsima <jerichoopsima22@gmail.com>
2022-09-23 18:54:53 +0800

    refactor

--- src/pages/clubs/[slug].tsx
+++ src/pages/clubs/[slug].tsx
@@ -1,35 +1,35 @@
-import Head from 'next/head';
-import { allClubs, Club } from 'contentlayer/generated'
+import Head from "next/head";
+import { allClubs, Club } from "contentlayer/generated";
 
 export default function ClubPage({ club }: { club: Club }) {
-    return (
-        <div>
-            <Head>
-                <title>{club.name}</title>
-                <meta name="description" content={club.description?.long ?? ''} />
-            </Head>
+	return (
+		<div>
+			<Head>
+				<title>{club.name}</title>
+				<meta name="description" content={club.description?.long ?? ""} />
+			</Head>
 
-            <h1>{club.name}</h1>
-            <p>{club.description?.long ?? ''}</p>
-            <div dangerouslySetInnerHTML={{ __html: club.body.html }}></div>
-        </div>
-    )
+			<h1>{club.name}</h1>
+			<p>{club.description?.long ?? ""}</p>
+			<div dangerouslySetInnerHTML={{ __html: club.body.html }}></div>
+		</div>
+	);
 }
 
 export async function getStaticPaths() {
-  const paths = allClubs.map(c => c.url);
+	const paths = allClubs.map((c) => c.url);
 
-  return {
-    paths,
-    fallback: false
-  }
+	return {
+		paths,
+		fallback: false,
+	};
 }
 
 export async function getStaticProps({ params: { slug } }) {
-    const club = allClubs.find(club => club._raw.flattenedPath === slug);
-    return {
-        props: {
-            club
-        }
-    }
+	const club = allClubs.find((club) => club._raw.flattenedPath === slug);
+	return {
+		props: {
+			club,
+		},
+	};
 }
