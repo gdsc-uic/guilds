835b5d73f61cab863a8702540f00b673d8abc385
Jericho Opsima <jerichoopsima22@gmail.com>
2022-09-23 17:29:33 +0800

    add montserrat & space grotesk font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,16 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { AppProps } from "next/app";
+import { extendTheme } from "@chakra-ui/react";
+import styled from "@emotion/styled";
 import "../styles/globals.css";
+import theme from "src/theme";
+import "@fontsource/space-grotesk";
+import "@fontsource/inter";
+import "@fontsource/montserrat";
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
-		<ChakraProvider>
+		<ChakraProvider theme={theme}>
 			<Component {...pageProps} />
 		</ChakraProvider>
 	);
---
+++ src/theme.ts
@@ -0,0 +1,10 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+	fonts: {
+		heading: `'Space Grotesk', sans-serif`,
+		body: `'Montserrat', sans-serif`,
+	},
+});
+
+export default theme;
