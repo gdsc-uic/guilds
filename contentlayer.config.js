05f9878624b745498f2ba5bf05482b9ebd8789e5
Jericho Opsima <jerichoopsima22@gmail.com>
2022-09-25 21:38:26 +0800

    refactor

--- contentlayer.config.js
+++ contentlayer.config.js
@@ -1,23 +1,46 @@
-import { defineDocumentType, defineNestedType, makeSource } from 'contentlayer/source-files';
+import {
+	defineDocumentType,
+	defineNestedType,
+	makeSource,
+} from "contentlayer/source-files";
 
-const ClubDescription = defineNestedType(() => ({
-    name: 'ClubDescription',
-    fields: {
-        long: {
-            type: 'string',
-            description: 'Full description of the club',
-            required: true
-        },
-        short: {
-            type: 'string',
-            description: 'Short description of the club'
-        }
-    }
+export const Club = defineDocumentType(() => ({
+	name: "Club",
+	filePathPattern: "**/*.md",
+	fields: {
+		name: {
+			type: "string",
+			description: "Name of the club",
+			required: true,
+		},
+		description: {
+			type: "nested",
+			of: ClubDescription,
+		},
+		tags: {
+			type: "list",
+			of: { type: "string" },
+		},
+		assets: {
+			type: "nested",
+			of: ClubAssets,
+		},
+		clubFaq: {
+			type: "nested",
+			of: ClubFaq,
+		},
+	},
+	computedFields: {
+		url: {
+			type: "string",
+			resolve: (club) => `/clubs/${club._raw.flattenedPath}`,
+		},
+	},
 }));
 
 const ClubFaq = defineNestedType(() => ({
 	name: "ClubFaq",
-    fields: {
+	fields: {
 		question: {
 			type: "string",
 			description: "Write a question that is frequently asked",
@@ -29,37 +52,34 @@ const ClubFaq = defineNestedType(() => ({
 	},
 }));
 
-export const Club = defineDocumentType(() => ({
-    name: 'Club',
-    filePathPattern: '**/*.md',
-    fields: {
-        name: {
-            type: 'string',
-            description: 'Name of the club',
-            required: true
-        },
-        description: {
-            type: 'nested',
-            of: ClubDescription
-        },
-        tags: {
-            type: 'list',
-            of: { type: 'string' }
-        },
-        assets: {
-            type: 'nested',
-            of: ClubAssets
-        }
-    },
-    computedFields: {
-        url: {
-            type: 'string',
-            resolve: (club) => `/clubs/${club._raw.flattenedPath}`
-        },
-    }
+const ClubDescription = defineNestedType(() => ({
+	name: "ClubDescription",
+	fields: {
+		long: {
+			type: "string",
+			description: "Full description of the club",
+			required: true,
+		},
+		short: {
+			type: "string",
+			description: "Short description of the club",
+		},
+	},
+}));
+
+const ClubAssets = defineNestedType(() => ({
+	name: "ClubAssets",
+	fields: {
+		logo: { type: "string" },
+		cover_photo: { type: "string" },
+		slideshows: {
+			type: "list",
+			of: { type: "string" },
+		},
+	},
 }));
 
 export default makeSource({
-    contentDirPath: 'src/clubs',
-    documentTypes: [Club]
+	contentDirPath: "src/clubs",
+	documentTypes: [Club],
 });
