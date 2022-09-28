66f23324bdac8461651a05873a6a12cb13d7b24e
Ned Palacios <npdoesmc@gmail.com>
2022-09-29 00:43:37 +0800

    fix: adjust container width, add homepage-specific background

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -3,12 +3,19 @@ import CurveBackground from "./CurveBackground";
 import Footer from "./Footer";
 import NavBar from "./NavBar";
 
-export default function Layout({ children, maxWidth = '85rem', bgColor = '#0057FF' }) {
+export default function Layout({ children, maxWidth = '90rem', bgColor = '#0057FF', isHome = false }) {
 	return (
 		<Box position="relative">
 			<Box width="full" zIndex="-1">
 				<CurveBackground fill={bgColor} />
 			</Box>
+			{isHome && <Box 
+				position="absolute" 
+				top="0" 
+				width="full" 
+				height="90rem" 
+				zIndex="-2" 
+				bgColor="#FFF6EA" />}
 			<Box>
 				<NavBar />
 				<Box mx="auto" px={maxWidth !== 'full' ? '1rem' : '0'} maxWidth={maxWidth}>
