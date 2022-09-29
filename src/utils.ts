b893fadeaa35330bc862fa3278f432681d2f5249
Ned Palacios <npdoesmc@gmail.com>
2022-09-29 14:21:52 +0800

    fix: slideshow image url

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,12 @@
 import { Club, ClubAssets } from "contentlayer/generated";
 
 export function clubAssetURL(club: Club, name: keyof ClubAssets | 'officer_images', filename?: string): string {
-	return `/club_assets/${club._raw.flattenedPath}/${club.assets[name] ?? name}${filename ? '/' + filename : ''}`;
+	let key = name;
+	if (key !== 'slideshows') {
+		key = club.assets[name];
+	}
+
+	return `/club_assets/${club._raw.flattenedPath}/${key}${filename ? '/' + filename : ''}`;
 }
 
 export const fetcher = (...args: [RequestInfo | URL, RequestInit?]) =>
