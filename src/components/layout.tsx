a836a9b0d28904ce7f37fce5864b89b3bac3f4ef
Ned Palacios <npdoesmc@gmail.com>
2022-09-30 00:56:06 +0800

    feat: add beta banner

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,4 +1,4 @@
-import { Box, Container } from "@chakra-ui/react";
+import { Box, Button, Container, HStack, Text } from "@chakra-ui/react";
 import CurveBackground from "./CurveBackground";
 import Footer from "./Footer";
 import NavBar from "./NavBar";
@@ -20,6 +20,31 @@ export default function Layout({ children, maxWidth = '90rem', bgColor = '#0057F
 				<NavBar />
 				<Box mx="auto" px={maxWidth !== 'full' ? '1rem' : '0'} maxWidth={maxWidth}>
 					{children}
+				</Box>
+
+				<Box position="sticky" bottom={0} insetX={0} bgColor="#FFE58A" zIndex={999}>
+					<HStack justifyContent={"center"} borderTop={"8px #000 solid"} py="4">
+						<Text fontFamily={"Space Grotesk"} fontSize="20" fontWeight={"bold"}>
+							Guilds is still in beta. Help us make it better!
+						</Text>
+
+						<Button
+							as="a"
+							href="https://docs.google.com/forms/d/e/1FAIpQLSekeADCSAqtMjjRiLEzMfcevWlLS9Az53bmo01zFgQJYeGfQg/viewform?usp=sf_link"
+							target="_blank"
+							bg="#F2779A"
+							color="white"
+							// px="55"
+							w="8rem"
+							py="4"
+							borderRadius="0"
+							borderWidth="4px"
+							borderColor="black"
+							boxSizing="border-box"
+						>
+							Feedback
+						</Button>
+					</HStack>
 				</Box>
 				<Footer />
 			</Box>
