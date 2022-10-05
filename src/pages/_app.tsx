ca9a2578d6aec057c5a4af9829e316a2f40c8ea7
Al Gabriel <jaworski.orig@gmail.com>
2022-10-05 12:42:00 +0800

    add: google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,16 +5,38 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "../styles/globals.css";
 import "@fontsource/space-grotesk";
 import "@fontsource/inter";
+import Script from "next/script";
+
 
 const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
+
 	return (
+    <>
 		<QueryClientProvider client={queryClient}>
 			<ChakraProvider theme={theme}>
+        <Script
+        strategy='afterInteractive'
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.MEASUREMENT_ID}`}
+      />
+      <Script id='ga-analytics'>
+        {
+          `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '${process.env.MEASUREMENT_ID}', {
+              page_path: window.location.pathname,
+            });
+          `
+        }
+      </Script>
 				<Component {...pageProps} />
 			</ChakraProvider>
 		</QueryClientProvider>
+    </>
 	);
 }
 
